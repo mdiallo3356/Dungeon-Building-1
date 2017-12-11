ae5832e61125411950b6a2b41a0b5c78a0083eec
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-12-11 09:13:52 -0500

    Blargh

---
+++ Dunegon 1/src/PoisonTrap.java
@@ -0,0 +1,20 @@
+public class PoisonTrap extends Room {
+
+
+
+	public PoisonTrap(int x, int y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public void enterRoom(Person x) {
+		// TODO Auto-generated method stub
+		occupant = x;
+		x.setxLoc(this.xLoc);
+		x.setyLoc(this.yLoc);
+		System.out.println("As you walk into the room, you hear the door behind you slamming shut. A gas begins to fill the room from small slits"
+				+ " in the walls.");
+	}
+	
+
+}
